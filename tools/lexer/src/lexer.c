49115c06beeba9305df8949ecd2e8fa249c1a64d
Phil Cooper-King <phil@cooperking.net>
2018-08-02 08:47:13 +0100

    lex tables

--- tools/lexer/src/lexer.c
+++ tools/lexer/src/lexer.c
@@ -3,8 +3,57 @@
 #include <expr/v_array.h>
 #include <string.h>
 
+const char  whitespace[] = {
+        ' ', '\t', '\n', '\r',
+        '\0'
+};
+
+const char numeric[] = {
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        '\0'
+};
+
+
+const char alpha[] = {
+        'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G',
+        'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N',
+        'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U',
+        'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z',
+        '\0'
+};
+
+
+const char str_literal[] = {
+        '\'', '\"',
+        '\0'
+};
+
 
 /* ----------------------------------------------------------- [ Helpers ] -- */
+
+
+static int
+contains(char needle, char *heystack)
+{
+        while (heystack) {
+                if (needle == *heystack) {
+                        return 1;
+                }
+
+                heystack += 1;
+        }
+
+        return 0;
+}
+
+static int is_whitespace(char c)    { return contains(c, whitespace); }
+static int is_alpha(char c)         { return contains(c, alpha); }
+static int is_numeric(char c)       { return contains(c, numeric); }
+static int is_alpha_numeric(char c) { return is_alpha(c) || is_numeric(c); }
+static int is_str_literal(char c)   { return contains(c, str_literal); }
+
+
+/* ----------------------------------------------------------- [ Parsers ] -- */
 
 
 static int
@@ -12,21 +61,27 @@ parse_num_literal(
         struct expr_token *next,
         const char *src)
 {
-        return 0;
+        const char *start = src;
+
+        if (!is_numeric(*src)) {
+                return 0;
+        }
+        
+        while (!is_whitespace(*src)) {
+                if (is_numeric(*src)) {
+                        src += 1;
+                }
+                else {
+                        return 0;
+                }
+        }
+
+        return src - start;
 }
 
 
 static int
 parse_hex_literal(
-        struct expr_token *next,
-        const char *src)
-{
-        return 0;
-}
-
-
-static int
-parse_bin_literal(
         struct expr_token *next,
         const char *src)
 {
@@ -62,6 +117,15 @@ parse_ident(
 
 
 static int
+parse_unknown(
+        struct expr_token *next,
+        const char *src)
+{
+        return 0;
+}
+
+
+static int
 parse_punct(
         struct expr_token *next,
         const char *src,
@@ -85,16 +149,15 @@ expr_lexer_create(
         int puntuation_len)
 {
         /* variables */
-        struct expr_token *token = 0; 
+        struct expr_token *token = 0;
 
         parser_fn parsers[] = {
-                parse_num_literal,
                 parse_hex_literal,
-                parse_bin_literal,
                 parse_flt_literal,
+                parse_num_literal,
                 parse_str_literal,
                 parse_ident,
-                0
+                0 /* terminator */
         };
 
         /* check good state */
@@ -119,12 +182,13 @@ expr_lexer_create(
                 }
 
                 if(consume == 0) {
-                        parse_punt(token, src);
+                        consume = parse_punct(token, src, puntuation, puntuation_len);
+                }
+                else {
+                        parse_unknown(token, src);
                 }
 
                 src += EXPR_MAX(1, consume);
-
-                /* new token */
         }
 
         return token;
